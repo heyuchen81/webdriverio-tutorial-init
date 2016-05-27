83c108e8f9d8ecfc13047551a6551ef647ad7440
Xin He <Xin.He@ingenta.com>
2016-05-27 16:41:45 +0100

    Initial commit.

---
+++ features/step_definitions/my-step.js
@@ -0,0 +1,27 @@
+/**
+ * to run these tests you need install Cucumber.js on your machine
+ * take a look at https://github.com/cucumber/cucumber-js for more informations
+ *
+ * first, install Cucumber.js via NPM
+ * $ npm install -g cucumber
+ *
+ * then go into the cucumber directory and start the tests with
+ * $ cucumber.js
+ */
+
+var assert = require('assert'),
+    tmpResult;
+
+module.exports = function(){
+
+    this.Given(/^I go on the website "([^"]*)"$/, function(url) {
+        browser.url(url);
+        
+    });
+
+    this.Then(/^should the title of the page be "([^"]*)"$/, function(expectedTitle) {
+        var title = browser.getTitle() + "15";
+        assert.equal(title, expectedTitle + "15", ' title is "'+ title + '" but should be "'+ expectedTitle);
+    });
+
+};
