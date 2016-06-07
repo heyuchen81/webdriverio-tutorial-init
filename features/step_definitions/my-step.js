a0bfa513b4e4c8fe02bca874ebff5ba8192273d8
Xin He <Xin.He@ingenta.com>
2016-06-07 18:18:46 +0100

    nth Commit: correct an assertion.

--- features/step_definitions/my-step.js
+++ features/step_definitions/my-step.js
@@ -22,7 +22,7 @@ module.exports = function(){
     this.Then(/^should the title of the page be "([^"]*)"$/, function(expectedTitle) {
         var title = browser.getTitle() + "15";
         console.log("THIS IS A CUSTOMISED LOG MADE BY XIN HE..........................")
-        assert.equal(title, expectedTitle, ' title is "'+ title + '" but should be "'+ expectedTitle);
+        assert.equal(title, expectedTitle + "15", ' title is "'+ title + '" but should be "'+ expectedTitle);
     });
 
 };
